c1b643c79abb7e2d5408b59841aa56f67f751b0b
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-14 14:54:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,9 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 1)
-    @@items << item
+    quantity.times do
+      @@items << item
+    end
     self.total += (price*quantity)
   end
 
