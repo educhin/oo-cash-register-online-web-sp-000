29b6da2140db732597638e181a3cb09a785d2234
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-14 14:59:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :employee_discount, :items
+  attr_accessor :total, :employee_discount, :items, :last_transaction_amount
 
   def initialize(employee_discount = 0, total = 0)
     @total = total
